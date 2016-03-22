e01955fa410b7a2ec65398fe3df258db16765892
angeltsh <YWY0719@163.com>
2016-03-22 16:59:29 +0800

    修改

--- src/main/java/com/integrated/controller/PageCtrl.java
+++ src/main/java/com/integrated/controller/PageCtrl.java
@@ -3,13 +3,21 @@ package com.integrated.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.integrated.entity.Test;
 
 @Controller
+@RequestMapping("")
 public class PageCtrl {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
 
 	@RequestMapping("{path}")
 	public String goPath(@PathVariable String path) {
-		System.out.println();
 		return path;
 	}
 }
